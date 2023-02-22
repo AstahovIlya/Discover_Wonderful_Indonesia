36854332146976b9c1d2e62a9bee8e91273fbd23
Ilya Astahov <ilia.astahoff@yandex.ru>
2023-02-23 00:11:49 +0300

    first commit

---
+++ js/index.js
@@ -0,0 +1,14 @@
+
+
+const swiper = new Swiper('.swiper', {
+   // Optional parameters
+   direction: 'horizontal',
+   loop: true,
+
+   // Navigation arrows
+   navigation: {
+      nextEl: '._icon-arrow_right',
+      prevEl: '._icon-arrow_left',
+   },
+
+});
